dcb11c0a9742a254ab692af115f4eb820c81dcce
Michael Jordan <mjthecoder53161@gmail.com>
2022-05-22 21:07:22 +0900

    Added routes for genres

---
+++ routes/genres.js
@@ -0,0 +1,64 @@
+const Joi = require('joi');
+const express = require('express');
+
+const router = express.Router();
+
+const genres = [
+    { id: 1, name: "Horror" },{ id: 2, name: "Romance" }, { id: 3, name: "Sci-Fi" }
+];
+
+router.get("/", (req, res) => {
+    res.send(genres)
+});
+
+router.get("/:id", (req, res) => {
+    const genre = genres.find(genre => genre.id == parseInt(req.params.id))
+    if (!genre) return res.status(404).send("Genre with given ID was not found!")
+    res.send(genre)
+});
+
+
+router.post("/", (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+    
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+
+    genres.append(genres)
+    res.send(genre)
+});
+
+
+router.put("/:id", (req, res) => {
+    const genre = genres.find(genre => genre.id == parseInt(req.params.id))
+    if (!genre) return res.status(404).send("Genre with given ID was not found")
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+
+});
+
+router.delete("/:id", (req, res) => {
+    const genre = genres.find(genre => genre.id == parseInt(req.params.id))
+    if (!genre) return res.status(404).send("Genre with given ID was not found")
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    res.send(genre);
+})
+
+
+function validateGenre(genre) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(genre)
+}
+
+module.exports = router;
