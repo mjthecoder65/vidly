27082601d72ffbe86948e9ebc0403886a2d70489
Michael Jordan <mjthecoder53161@gmail.com>
2022-05-25 22:37:36 +0900

    Connected vidly to MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const mongoose = require('mongoose');
 const genresRouter = require("./routes/genres");
 const express = require("express");
 
 const app = express();
 app.use(express.json());
-app.use("/api/genres", genresRouter);
 
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Connected to MongoDB....."))
+.catch(err => console.log(err.message))
+
+app.use("/api/genres", genresRouter);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
