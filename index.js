4f4e8fad5738ab2bc23d117838345393d4884c23
Michael Jordan <mjthecoder53161@gmail.com>
2022-05-26 20:43:49 +0900

    Configured joi-objectid

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
 const genresRouter = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals")
+const mongoose = require('mongoose');
 const express = require("express");
+
 
 const app = express();
 app.use(express.json());
@@ -12,6 +18,8 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use("/api/genres", genresRouter);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
