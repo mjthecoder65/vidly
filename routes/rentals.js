d70ddf3723d64d29b61b8fee25c093ba7866be62
Michael Jordan <mjthecoder53161@gmail.com>
2022-05-26 20:46:44 +0900

    Added rental routes

---
+++ routes/rentals.js
@@ -0,0 +1,64 @@
+const { Rental, validateRental } = require("../models/rental");
+const { Movie } = require("../models/movie");
+const { Customer } = require("../models/customer");
+const mongoose = require("mongoose");
+const express = require("express")
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    const rentals = await Rental.find().sort("-dateOut");
+    res.send(rentals)
+});
+
+router.post("/", async (req, res) => {
+    const { error } = validateRental(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const customer = await Customer.findById(req.body.customerId);
+    if (!customer) return res.status(400).send("Invalid customer.");
+
+    movie = await Movie.findById(req.body.movieId);
+    if (!movie) return res.status(404).send("Invalid Movie.")
+
+    if (movie.numberInStock == 0) return res.status(400).send("Movie not in stock.");
+
+    let rental = new Rental({
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            phone: customer.phone
+        },
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate
+        }
+    });
+
+    // TODO:
+    // WE NEED TRANSACTION HERE. NOSQL HAS NO TRANSACTION
+    // TWO PHASE COMMIT: MONGO DB
+    // NPM PACKAGE THAT SIMULATE TRANSACTION IN MONGO.
+    // ATOMIC: THEY SHOULD EITHER COMPLETE SUCCESSFULLY OR ROLLBACK.
+
+    rental = await rental.save();
+    
+    movie.numberInStock--;
+    await movie.save();
+    
+    res.send(rental)
+});
+
+router.delete("/:id", async (req, res) => { 
+    const rental = await Rental.findByIdAndRemove(req.params.id)
+    if (!rental) return res.status(404).send("Rental with given ID was not found")
+    res.send(rental);
+})
+
+router.get("/:id", async (req, res) => {
+    const rental = await Rental.findById(req.params.id);
+    if (!rental) return res.status(404).send("Rental with given ID was not found!")
+    res.send(rental)
+});
+
+module.exports = router;
