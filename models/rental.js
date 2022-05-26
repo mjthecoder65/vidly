3e2ede3a4d07dc77854b8bf2ab76fac3eb57d615
Michael Jordan <mjthecoder53161@gmail.com>
2022-05-26 20:46:18 +0900

    Added rental model

---
+++ models/rental.js
@@ -0,0 +1,75 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+
+const rentalSchema = new mongoose.Schema({
+    customer: {
+        type: new mongoose.Schema({
+            name: {
+                type: String,
+                required: true,
+                trim: true,
+                minLength: 5, 
+                maxLength: 50
+            },
+            isGold: {
+                type: Boolean,
+                default: false
+            },
+            phone: {
+                type: String,
+                required: true,
+                trim: true,
+                minLength:5,
+                maxLength:50
+            }
+        }),
+        required: true
+    },
+    movie: {
+        type: new mongoose.Schema({
+            title: {
+                type: String,
+                required: true,
+                trim: true,
+                minLength: 5,
+                maxLength: 255
+            },
+            dailyRentalRate: {
+                type: Number,
+                min: 0,
+                max: 255,
+                required: true
+            }
+
+        }),
+        required: true
+    },
+    dateOut: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    dateReturned: {
+        tyep: Date
+    },
+    rentalFee: {
+        type: Number,
+        min: 0
+    }
+})
+
+
+const Rental = mongoose.model("Rental", rentalSchema);
+
+function validateRental(rental) {
+    const schema = Joi.object({
+        customerId: Joi.objectId().required(),
+        movieId: Joi.objectId().required()
+    });
+
+    return schema.validate(rental)
+}
+
+
+module.exports = {Rental, validateRental };
