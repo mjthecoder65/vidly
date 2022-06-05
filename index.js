874c2f02252418cb69fb2bef69dd553b01a21ae2
Michael Jordan <mjthecoder53161@gmail.com>
2022-06-05 21:33:21 +0900

    Updated index.js

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-
-const genresRouter = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals")
-const mongoose = require('mongoose');
 const express = require("express");
-
-
 const app = express();
-app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("Connected to MongoDB....."))
-.catch(err => console.log(err.message))
-
-app.use("/api/genres", genresRouter);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
+require('./startup/logging')();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config");
+require('./startup/validation');
+require('./startup/prod')(app);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
