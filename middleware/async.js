32f4a2f10b0dd190ca0555236c947252c305be74
Michael Jordan <mjthecoder53161@gmail.com>
2022-06-05 21:35:46 +0900

    Added a wrapper for error handling

---
+++ middleware/async.js
@@ -0,0 +1,10 @@
+module.exports = function (handler) {
+    // A wrapper function for router handler.
+    return async (req, res, next) => {
+        try {
+            await handler(res, req);
+        } catch(err) {
+            next(err)
+        }
+    }
+}
